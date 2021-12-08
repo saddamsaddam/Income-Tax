61d4f5acb10f9bc1736400609acd00520082ba64
saddamsaddam <56682452+saddamsaddam@users.noreply.github.com>
2021-12-09 00:55:16 +0600

    Create Boxlayout.java

---
+++ Boxlayout.java
@@ -0,0 +1,8 @@
+         p1=new JPanel();
+        // p1.setLayout(new GridLayout(47,1));
+         box=new BoxLayout(p1,BoxLayout.Y_AXIS);
+         p1.setLayout(box);
+         
+     p1.add(btn1);
+          p1.add(Box.createVerticalStrut(50));
+        
